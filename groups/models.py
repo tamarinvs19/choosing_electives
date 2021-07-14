3fd38c8bd57dd13a87c4d10e112e73b0f79438b9
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-07-14 13:34:55 +0300

    Add student subgroups

--- groups/models.py
+++ groups/models.py
@@ -15,3 +15,28 @@ class Teachers(Group):
 class Students(Group):
     """Student group"""
     pass
+
+
+class TheThirdYear(Group):
+    """The third year students"""
+    pass
+
+
+class TheFourthYear(Group):
+    """The fourth year students"""
+    pass
+
+
+class Mathematics(Group):
+    """Students on Mathematics program"""
+    pass
+
+
+class DataScience(Group):
+    """Students on DataScience program"""
+    pass
+
+
+class ModernProgramming(Group):
+    """Students on ModernProgramming (SP) program"""
+    pass
