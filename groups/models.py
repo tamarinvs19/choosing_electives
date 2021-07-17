43a5f62496116c5c88dfc38e33a1eb25494cecbb
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-07-17 15:27:24 +0300

    remove a few group classes

--- groups/models.py
+++ groups/models.py
@@ -2,21 +2,6 @@
 from django.contrib.auth.models import Group
 
 
-class Administrations(Group):
-    """Administration group"""
-    pass
-
-
-class Teachers(Group):
-    """Teacher group"""
-    pass
-
-
-class Students(Group):
-    """Student group"""
-    pass
-
-
 class YearOfEducation(Group):
     """One year students"""
     pass
