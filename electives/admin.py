c322567ddd7ed26e3ec5cc01001ce4bbe2d0e02d
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-07-17 14:59:45 +0300

    Update admin for electives

--- electives/admin.py
+++ electives/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from .models import Elective
+from .models import Elective, StudentOnElective, TeacherOnElective
+
+
+class StudentOnElectiveInline(admin.TabularInline):
+    model = StudentOnElective
+    extra = 2
+
+
+class TeacherOnElectiveInline(admin.TabularInline):
+    model = TeacherOnElective
+    extra = 1
 
 
 class ElectiveAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['name', 'credit_unit']}),
     ]
+    inlines = [StudentOnElectiveInline, TeacherOnElectiveInline]
     list_display = ('name', 'credit_unit')
     list_filter = ['credit_unit']
     search_fields = ['name']
