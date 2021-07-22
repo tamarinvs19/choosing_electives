95390edb7efd4728a01f38139d615c1a9acf3c5f
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-07-22 12:16:06 +0300

    add elective page

--- electives/urls.py
+++ electives/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'electives'
 urlpatterns = [
     path('', views.open_elective_list, name='elective_list'),
+    path('<int:elective_id>/', views.open_elective_page, name='elective_page'),
 ]
 
--- electives/views.py
+++ electives/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .controller import Controller
+from .models import Elective
 
 
 def open_elective_list(request, **kwargs):
     context = {'electives': Controller.get_all_electives()}
     return render(request, 'electives/elective_list.html', context)
+
+
+def open_elective_page(request, elective_id, **kwargs):
+    context = {'elective': Elective.objects.get(id=elective_id)}
+    return render(request, 'electives/elective_page.html', context)
 
