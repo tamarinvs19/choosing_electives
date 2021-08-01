745c3ac2eb7cf119b2bff9d1425ca608bb0d3d00
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-08-01 08:52:07 +0300

    update connection with env

---
+++ my_environ.py
@@ -0,0 +1,22 @@
+class Env(object):
+    def __init__(self, **kwargs):
+        self.VARS = {key: value[0] for key, value in kwargs.items()}
+        self.TYPES = {key: value[1] for key, value in kwargs.items()}
+
+    def read_env(self, file_name='.env'):
+        with open(file_name, 'r') as env_file:
+            for line in env_file.readlines():
+                line = line.strip()
+                if '=' in line:
+                    key, value = line.split('=', maxsplit=1)
+                    if key in self.TYPES:
+                        self.VARS[key] = self.TYPES[key](value)
+                    else:
+                        self.VARS[key] = value
+
+    def __getitem__(self, key):
+        if key in self.VARS:
+            return self.VARS[key]
+        else:
+            raise KeyError('Key {key} did not exists in the enviroment'.format(key=key))
+
