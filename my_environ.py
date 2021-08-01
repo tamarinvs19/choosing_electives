e5479397c533442cec25843d89a7575fbf25b77b
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-08-01 09:13:32 +0300

    update working with SECRET_KEY

--- my_environ.py
+++ my_environ.py
@@ -1,18 +1,22 @@
+import os
+
+
 class Env(object):
     def __init__(self, **kwargs):
         self.VARS = {key: value[0] for key, value in kwargs.items()}
         self.TYPES = {key: value[1] for key, value in kwargs.items()}
 
     def read_env(self, file_name='.env'):
-        with open(file_name, 'r') as env_file:
-            for line in env_file.readlines():
-                line = line.strip()
-                if '=' in line:
-                    key, value = line.split('=', maxsplit=1)
-                    if key in self.TYPES:
-                        self.VARS[key] = self.TYPES[key](value)
-                    else:
-                        self.VARS[key] = value
+        if os.path.exists(file_name):
+            with open(file_name, 'r') as env_file:
+                for line in env_file.readlines():
+                    line = line.strip()
+                    if '=' in line:
+                        key, value = line.split('=', maxsplit=1)
+                        if key in self.TYPES:
+                            self.VARS[key] = self.TYPES[key](value)
+                        else:
+                            self.VARS[key] = value
 
     def __getitem__(self, key):
         if key in self.VARS:
