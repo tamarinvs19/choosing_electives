42e1a7cf94a25d6972a64059d7952d54d819aae4
Vyacheslav Tamarin <tamarin.vs@ya.ru>
2021-11-13 16:53:49 +0300

    Update view for user pages

--- users/views.py
+++ users/views.py
@@ -1,13 +1,20 @@
+
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from electives.models import Elective
+from electives.models import Elective, StudentOnElective
 
 
-def open_personal_page(request, user_id, **kwargs):
+@login_required
+def open_personal_page(request, **kwargs):
     context = {}
     return render(request, 'account/personal_page.html', context)
 
 
+@login_required
 def open_sorting_page(request, user_id, **kwargs):
-    context = {'electives': Elective.objects.all()}
+    applications = StudentOnElective.objects.filter(student=user_id).all()
+    context = {
+        'applications': applications
+    }
     return render(request, 'account/sort_electives.html', context)
 
