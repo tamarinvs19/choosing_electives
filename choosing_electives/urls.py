ab537d1b9da13374f52ab8f11aea3f0aea46d502
Vyacheslav Tamarin <tamarin.vs@ya.ru>
2022-02-26 11:03:11 +0300

    Add redirect from root url

--- choosing_electives/urls.py
+++ choosing_electives/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
-    path('electives/admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('electives/', include('apps.electives.urls')),
+    path('electives/admin/', admin.site.urls),
     path('electives/users/', include('apps.users.urls')),
     path('electives/accounts/', include('allauth.urls')),
     path('electives/parsing/', include('apps.parsing.urls')),
