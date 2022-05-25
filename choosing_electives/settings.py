f706bdcfbfadc293b6a546750b8293956197e63b
Vyacheslav Tamarin <tamarin.vs@ya.ru>
2022-05-25 16:59:23 +0300

    return postgres

--- choosing_electives/settings.py
+++ choosing_electives/settings.py
@@ -118,9 +118,18 @@ WSGI_APPLICATION = 'choosing_electives.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_NAME') or env['POSTGRES_NAME'],
+        'USER': os.environ.get('POSTGRES_USER') or env['POSTGRES_USER'],
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD') or env['POSTGRES_PASSWORD'],
+        'HOST': os.environ.get('POSTGRES_HOST') or env['POSTGRES_HOST'],
+        'PORT': '5432',
+        'CONN_MAX_AGE': 60,
+        'OPTIONS': {
+            'connect_timeout': 10,
+            'options': '-c statement_timeout=15000ms',
+        },
     }
 }
 
